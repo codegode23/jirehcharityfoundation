419e1d99d5daba7f71d87345e1ca8da2487835c7
Jasper Mawunyo Kojo Akabua <jasperakabua020@gmail.com>
2023-02-25 20:41:17 +0000

    New changes added

--- js/script.js
+++ js/script.js
@@ -108,4 +108,23 @@ $(".step08").click( function() {
 
 //gallery section ends
 
-var s = skrollr.init();
+//Reveal on Scroll
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+
+    for(var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 150;
+
+        if(revealtop < windowHeight - revealPoint) {
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    } 
+}
