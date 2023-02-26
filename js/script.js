52e72417d8ac7bc0f57b35e79f0253ef6d46d02f
Jasper Mawunyo Kojo Akabua <jasperakabua020@gmail.com>
2023-02-26 12:52:13 +0000

    Fixed Filterable issue

--- js/script.js
+++ js/script.js
@@ -89,29 +89,8 @@ $(".step08").click( function() {
 	$(".analysis").addClass("active").siblings().removeClass("active");
 });
 
+
  
-
- //gallery section starts
-
- $(document).ready(function(){
-
-    $(".buttons").click(function(){
-
-        $(this).addClass('active').siblings().removeClass('active');
-
-        var filter = $(this).attr('data-filter');
-
-        if(filter == 'all'){
-            $('.image').show(400);
-        }else{
-            $('.image').not('.'+filter).hide(200);
-            $('.image').filter('.'+filter).show(400);
-        }
-
-    });
-
-
-});
 
 //Reveal on Scroll
 
