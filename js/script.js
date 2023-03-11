4c88d4ff3c368540c509415b8b2ca04c6b1dbc36
Jasper Mawunyo Kojo Akabua <jasperakabua020@gmail.com>
2023-03-11 18:28:39 +0000

    Made some few modifications to the story Section

--- js/script.js
+++ js/script.js
@@ -39,57 +39,6 @@ btn.addEventListener("click", () =>{
 });
 });
 
-
-
-
-//Script for the story section
-
-$(".step").click( function() {
-	$(this).addClass("active").prevAll().addClass("active");
-	$(this).nextAll().removeClass("active");
-});
-
-$(".step01").click( function() {
-	$("#line-progress").css("width", "0%");
-	$(".discovery").addClass("active").siblings().removeClass("active");
-});
-
-$(".step02").click( function() {
-	$("#line-progress").css("width", "15%");
-	$(".strategy").addClass("active").siblings().removeClass("active");
-});
-
-$(".step03").click( function() {
-	$("#line-progress").css("width", "30%");
-	$(".creative").addClass("active").siblings().removeClass("active");
-});
-
-$(".step04").click( function() {
-	$("#line-progress").css("width", "45%");
-	$(".production").addClass("active").siblings().removeClass("active");
-});
-
-$(".step05").click( function() {
-	$("#line-progress").css("width", "60%");
-	$(".analysis").addClass("active").siblings().removeClass("active");
-});
-
-$(".step06").click( function() {
-	$("#line-progress").css("width", "75%");
-	$(".analysis").addClass("active").siblings().removeClass("active");
-});
-
-$(".step07").click( function() {
-	$("#line-progress").css("width", "90%");
-	$(".analysis").addClass("active").siblings().removeClass("active");
-});
-
-$(".step08").click( function() {
-	$("#line-progress").css("width", "105%");
-	$(".analysis").addClass("active").siblings().removeClass("active");
-});
-
-
  
 
 //Reveal on Scroll
@@ -112,3 +61,5 @@ function reveal() {
         }
     } 
 }
+
+
